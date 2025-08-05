4acbf5b64cc064f0a579d4fe0cb8af6743a78d4c
rakshhx <145245132+rakshhx@users.noreply.github.com>
2025-08-05 21:37:20 +0530

    Create Solutions_Application.java

---
+++ Binary Search/Solutions_Application.java
@@ -0,0 +1,37 @@
+//Application(Super imp)	
+
+//71.	Aggressive cows
+
+
+//72.	Allocate minimum pages
+
+
+//73.	Painter partition
+
+
+//74.	Split array largest sum
+
+
+//75.	Minimum num of days to make m bouquets
+
+
+//76.	Capacity to ship packages within D days
+
+
+//77.	koko eating bananas
+
+
+//78.	kth smallest number in matrix multiplication table
+
+
+//79.	kth smallest pair distance
+
+
+//80.	Ugly number II
+
+
+//81.	Median of 2 sorted arrays
+
+
+//82.	Find smallest divisor given threshold
+
