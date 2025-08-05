17c185a3f548a09f455302cc08760409b8692430
rakshhx <145245132+rakshhx@users.noreply.github.com>
2025-08-05 21:34:59 +0530

    Create Solutions_Rotated_Array.java

---
+++ Binary Search/Solutions_Rotated_Array.java
@@ -0,0 +1,13 @@
+//Sorted rotated array	
+
+//60.	Search element in sorted rotated array(With and without duplicate)
+
+
+//61.	Minimum element in sorted rotated array(With and without duplicate)
+
+
+//63.	Number of times array is sorted
+
+
+//64.	Maximum element in sorted rotated array
+
