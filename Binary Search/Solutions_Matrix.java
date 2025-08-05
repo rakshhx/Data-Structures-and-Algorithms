ea9bd67a2e64293ae2e1dc5301eaaae57e1cbf1b
rakshhx <145245132+rakshhx@users.noreply.github.com>
2025-08-05 21:36:15 +0530

    Create Solutions_Matrix.java

---
+++ Binary Search/Solutions_Matrix.java
@@ -0,0 +1,14 @@
+//Matrix	
+
+//67.	Find row with maximum number of 1's
+
+
+//68.	Search in row wise column wise sorted matrix
+
+
+//69.	Search in sorted matrix II
+
+
+//70.	Find peak in sorted matrix
+
+
