307e4aded668cefd4ad28eb52734a21fd6ffea6f
rakshhx <145245132+rakshhx@users.noreply.github.com>
2025-08-05 21:38:03 +0530

    Update Solutions_Matrix.java

--- Binary Search/Solutions_Matrix.java
+++ Binary Search/Solutions_Matrix.java
@@ -5,6 +5,31 @@
 
 //68.	Search in row wise column wise sorted matrix
 
+class Solution {
+    public boolean searchMatrix(int[][] mat, int x) {
+        // code here
+        int n =mat.length;
+        int m =mat[0].length;
+        boolean ans=false;
+        int low=0,high=(n*m)-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int row=mid/m;
+            int col=mid%m;
+            if(mat[row][col] == x){
+                ans=true;
+                break;
+            }
+            else if(mat[row][col] > x){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+}
 
 //69.	Search in sorted matrix II
 
